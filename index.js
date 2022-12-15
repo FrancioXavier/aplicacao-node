8703176679d6bf3eec86bf55381cd7a59ca6bd77
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2022-12-15 09:41:33 -0300

    Creating data, redirect and home.handlebars

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const Post = require('./models/Post');
 
 //CONFIG
     //Template engine
-        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
 
     //Body Parser
     app.use(bodyParser.urlencoded({extended: false}));
@@ -14,15 +15,25 @@ const bodyParser = require("body-parser")
 
 //ROTAS
 
-    app.get('/cadastro', function(req, res){
-        res.render('form') //renderiza um template handlebars
+    app.get('/', function(req, res){
+        res.render('home');
+        
     })
 
+    app.get('/cadastro', function(req, res){
+        res.render('form'); //renderiza um template handlebars
+    });
+
     app.post('/poscad', function(req, res){
-        let titulo = req.body.titulo; //pegando dados do formulario com o body parser
-        let conteudo = req.body.conteudo;
-        res.send(`Titulo: ${titulo} <br> Conteúdo: ${conteudo}`)
-    })//só pode ser acessada através da chamada do method: POOST
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/');
+        }).catch(function(erro){
+            res.send(`Erro ao criar postagem: ${erro}`);
+        })
+    }); //só pode ser acessada através da chamada do method: POOST
 
 
 
